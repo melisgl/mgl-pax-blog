c240a81e2a0db1c7e50dc199ae514ba1fc4d8bb3
Gabor Melis <(none)>
2022-12-06 14:35:18 +0000

    more

--- src/blog.lisp
+++ src/blog.lisp
@@ -135,8 +135,8 @@
          (*document-html-head*
            (lambda ()
              (format nil "~A~A"
-                     "<link href='https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,500;0,700;1,500;1,700&display=swap' rel='stylesheet'>
-<style> @import url('https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,500;0,700;1,500;1,700&display=swap'); </style>
+                     "<link rel='preconnect' href='https://fonts.googleapis.com'>
+<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
 <link href='https://fonts.googleapis.com/css2?family=Gentium+Book+Plus:ital,wght@0,400;0,700;1,400;1,700&display=swap' rel='stylesheet'>
 <style> @import url('https://fonts.googleapis.com/css2?family=Gentium+Book+Plus:ital,wght@0,400;0,700;1,400;1,700&display=swap'); </style>
 "
@@ -3114,6 +3114,9 @@
   ==> #<TRY:TRIAL (TRY-SUITE) UNEXPECTED-FAILURE 0.004s ⊠1>
   ```
   """)
+
+(defpost @zooming-into-fonts (:title "Zooming into Fonts")
+  "")
 
 #+nil
 (defpost @tta-practioner
