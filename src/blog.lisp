d39f8727c7fde701c7da8db6c9028d5cbcd1347b
Gabor Melis <mega@retes.hu>
2025-02-11 20:49:19 +0100

    m

--- src/blog.lisp
+++ src/blog.lisp
@@ -166,49 +166,7 @@
 
   gtag('config', 'G-7X64Q1D73F');
 </script>
-<script>
-function padToBaselineGrid() {
-  document.querySelectorAll('pre, img, #toc').forEach(el => {
-    if (el.alt == 'end-of-post' || el.alt == 'about-me-die') {
-      return;
-    }
-    // We either add a wrapper div and pad that or pad the element
-    // itself.
-    let padSelf = (el.id == 'toc');
-    let currentHeight = el.getBoundingClientRect().height;
-    if (padSelf) {
-      var paddingTop = parseFloat(window.getComputedStyle(el).getPropertyValue('padding-top'));
-      var paddingBottom = parseFloat(window.getComputedStyle(el).getPropertyValue('padding-bottom'));
-      currentHeight -= paddingTop + paddingBottom;
-    }
-    let lineHeight = parseFloat(window.getComputedStyle(document.body).lineHeight);
-    let targetHeight = Math.ceil(currentHeight / lineHeight) * lineHeight;
-    let extraPadding = targetHeight - currentHeight;
-    if (padSelf) {
-      elToPad = el;
-    } else {
-      if (el.parentNode.className != 'padding-wrapper') {
-        w = document.createElement('div');
-        w.className = 'padding-wrapper';
-        el.replaceWith(w);
-        w.appendChild(el);
-      }
-      if (extraPadding < 0.75*lineHeight) {
-        extraPadding += lineHeight;
-      }
-      elToPad = el.parentNode;
-    }
-    // Specify padding in rem, so that it scales with font size
-    // changes.
-    let remPx = parseFloat(getComputedStyle(document.documentElement).fontSize)
-    extraPadding = extraPadding / 2 /remPx;
-    elToPad.style.paddingTop = `${extraPadding}rem`;
-    elToPad.style.paddingBottom = `${extraPadding}rem`;
-  });
-}
-window.addEventListener('load', padToBaselineGrid);
-window.addEventListener('resize', padToBaselineGrid);
-</script>
+<script src='grid.js'></script>
 <link rel='shortcut icon' type='image/png' href='favicon.png'>
 <link rel='preconnect' href='https://fonts.googleapis.com'>
 <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
